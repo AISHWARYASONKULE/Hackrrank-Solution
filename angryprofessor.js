13ab04b1a4e0ab59df0197bf430c88eedc5f2c32
AISHWARYASONKULE <92326845+AISHWARYASONKULE@users.noreply.github.com>
2022-07-13 11:45:19 +0530

    Add files via upload

---
+++ angryprofessor.js
@@ -0,0 +1,71 @@
+//A Discrete Mathematics professor has a class of students. Frustrated with their lack of discipline, the professor decides to cancel class if fewer than some number of students are present when class starts. Arrival times go from on time () to arrived late ().
+
+//Given the arrival time of each student and a threshhold number of attendees, determine if the class is cancelled.
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'angryProfessor' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY a
+ */
+
+function angryProfessor(k, a) {
+    // Write your code here
+const YES = "YES";
+    const NO  = "NO";
+
+    let inClassCount = 0; // keeping running total
+    for(const arrivalTime of a){ // looping through each arrivalTime; O(n)
+        inClassCount += arrivalTime <= 0; // get boolean (true/false) if student is late or on time, adding (1/0) to total
+        if(inClassCount >= k) return NO; // before loop is done, if the total count is past threshold, stop and return NO
+    }
+    return YES; // loop finished without issue, reutrn YES; Time Complexity = O(n)
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const n = parseInt(firstMultipleInput[0], 10);
+
+        const k = parseInt(firstMultipleInput[1], 10);
+
+        const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+        const result = angryProfessor(k, a);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
